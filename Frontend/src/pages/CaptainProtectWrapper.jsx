c18ff81fd2f87ba0f2c60d61734db9a8f1d6dce6
Hritvik Tayal <hritviktayal74@gmail.com>
2025-01-17 17:16:53 +0530

    created centralised authentication feature

--- Frontend/src/pages/CaptainProtectWrapper.jsx
+++ Frontend/src/pages/CaptainProtectWrapper.jsx
@@ -1,6 +1,7 @@
-import React, {useContext,useEffect} from 'react'
+import React, {useContext,useEffect, useState} from 'react'
 import { useNavigate } from 'react-router-dom'
 import { CaptainDataContext } from '../context/CaptainContext';
+import axios from 'axios'
 
 
 const CaptainProtectWrapper = ({
@@ -10,12 +11,40 @@ const CaptainProtectWrapper = ({
     const navigate = useNavigate();
     const token = localStorage.getItem('token')
     const {captain, setCaptain} = useContext(CaptainDataContext)
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(()=>{
-      if(!token){
+    if(!token){
         navigate('/captain-login');
     }
+
+        // Profile is fetched here as user and captain both will save token in localStorage so as to prevent unauthorized access of user as captain we will fetch the captainProfile and if the token authenticates for captain profile then we let he user log in as captain
+        axios.get(`${import.meta.env.VITE_BASE_URL}/captain/profile`,{
+            headers:{
+                Authorization: `Bearer ${token}`
+            }
+        }).then((response)=>{
+            if(response.status === 201){
+                setCaptain(response.data.captain)
+                setIsLoading(false)
+            }
+        }).catch(err => { // if token not validated as captain then error and back to login
+            console.log(err);
+            localStorage.removeItem('token') //token removed 
+            navigate('/captain-login')
+            
+        })
     },[token])
+
+
+    if(isLoading){
+        return (
+            <>
+            Loading...
+            </>
+        )
+    }
+
   return (
     <div>
       {children}
--- Frontend/src/pages/UserprotectWrapper.jsx
+++ Frontend/src/pages/UserprotectWrapper.jsx
@@ -1,25 +1,44 @@
-import React, {useContext,useEffect} from 'react'
-import { UserDataContext } from '../context/UserContext'
-import { useNavigate } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from "react";
+import { UserDataContext } from "../context/UserContext";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const UserprotectWrapper = ({
-    children
-}) => {
-    
-    const navigate = useNavigate();
-    const token = localStorage.getItem('token')
+const UserprotectWrapper = ({ children }) => {
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+  const { user, setUser } = useContext(UserDataContext);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(()=>{
-      if(!token){
-        navigate('/login');
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
     }
-    },[token])
-  return (
-    <div>
-      {children}
-    </div>
-  )
-}
+
+    axios
+      .get(`${import.meta.env.VITE_BASE_URL}/users/profile`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setUser(response.data.user);
+          setIsLoading(false);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        localStorage.removeItem("token");
+        navigate("/login");
+      });
+  }, [token]);
+
+  if (isLoading) {
+    return <>Loading...</>;
+  }
+
+  return <div>{children}</div>;
+};
 
 export default UserprotectWrapper;
 
