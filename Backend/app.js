2c856a3a71f0b604f1e28aaff6e806217755cb4c
Hritvik Tayal <hritviktayal74@gmail.com>
2025-02-01 15:41:35 +0530

    created api endpoints for map service

--- Backend/app.js
+++ Backend/app.js
@@ -7,6 +7,7 @@ const cookieParser = require('cookie-parser')
 
 const userRoute = require('./routes/user.routes.js');
 const captainRoute = require('./routes/captain.routes.js')
+const mapsRoutes = require('./routes/map.routes.js')
 
 app.use(cors());
 app.use(cookieParser())
@@ -17,5 +18,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/users',userRoute)
 app.use('/captain',captainRoute);
+app.use('/maps',mapsRoutes)
 
 module.exports = app;
